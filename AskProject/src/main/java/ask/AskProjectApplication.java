d828837750b402b598a603844b4a2de4b60a3faf
SoyunJu <116853739+SoyunJu@users.noreply.github.com>
2023-01-12 20:36:45 +0900

    ㅇㄴㅁㅎ
    ㄴㅁㅇㅎㅎㄴㅇㅁㅎ

---
+++ AskProject/src/main/java/ask/AskProjectApplication.java
@@ -0,0 +1,14 @@
+package ask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class AskProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskProjectApplication.class, args);
+	}
+
+}
---
+++ AskProject/src/main/java/ask/dto/AskDto.java
@@ -0,0 +1,17 @@
+package ask.dto;
+
+import lombok.Data;
+
+@Data
+public class AskDto {
+
+	private int askIdx;
+	private String title;
+	private String contents;
+	private int hitCnt;
+	private String registDt;
+	private String registId;
+	private String updatedDt;
+	private String updatedId;
+
+}
